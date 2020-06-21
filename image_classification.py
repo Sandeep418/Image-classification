6d778ae82b28d682fe911068b2f16e104f804711
Sandeep G <40691260+Sandeep418@users.noreply.github.com>
2020-06-22 01:53:40 +0530

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -25,7 +25,7 @@ data_path = DATA_HOME_DIR + '/'
 split_train_path = data_path + '/train/'
 full_train_path = data_path + '/train_full/'
 valid_path = data_path + '/valid/'
-test_path = 'gfgfdgf'#'C:/Users/gsand/Downloads/train_SOaYf6m/train/1/'#DATA_HOME_DIR + '/test/test/'
+test_path = ''#'C:/Users/gsand/Downloads/train_SOaYf6m/train/1/'#DATA_HOME_DIR + '/test/test/'
 saved_model_path = ROOT_DIR + '/models/'
 submission_path = ROOT_DIR + '/submissions/'
 
@@ -43,54 +43,6 @@ archs = ["resnext101_32x8d"]
 model_names = sorted(name for name in models.__dict__ if name.islower() and not name.startswith("__"))
 print(model_names)
 best_prec1 = 0
-
-
-class CustomDataset():
-    def __init__(self, csv_file,root_dir, sufix=None, transform=None):
-        """
-        Args:
-            csv_file   (string):             Path to the csv file with annotations.
-            root_dir   (string):             Directory with all the images.
-            id_col     (string):             csv id column name.
-            target_col (string):             csv target column name.
-            sufix      (string, optional):   Optional sufix for samples.
-            transform  (callable, optional): Optional transform to be applied on a sample.
-        """
-        self.data = pd.read_csv(csv_file)
-       # self.id = id_col
-       # self.target = target_col
-        self.root = root_dir
-        self.sufix = sufix
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        # get the image name at the different idx
-        img_name = self.data.loc[idx, self.id]
-
-        # if there is not sufic, nothing happened. in this case sufix is '.jpg'
-        if self.sufix is not None:
-            img_name = img_name + self.sufix
-
-        # it opens the image of the img_name at the specific idx
-        image = Image.open(os.path.join(self.root, img_name))
-
-        # if there is not transform nothing happens, here we defined below two transforms for train and for test
-        if self.transform is not None:
-            image = self.transform(image)
-
-        # define the label based on the idx
-        # label = self.data.loc[idx, self.target].values
-        # label = torch.from_numpy(label.astype(np.int8))
-        # label = label.squeeze(-1)
-
-        # Test second option
-
-        label_test = self.data.iloc[idx, 1:5].values.astype('float32')
-
-        return image, label_test
 
 
 def test(test_loader, model):
